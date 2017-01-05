41cc7b1d397194eb5fc8b6d31f5a54bcfb002226
Haskaalo <haskaalo@outlook.com>
2017-01-05 15:08:07 -0500

    better stats

--- stats.js
+++ stats.js
@@ -1,13 +1,15 @@
 // NOT COMPLETED
+function search() {
 var request = require('request');
- var replace = result.battletag.replace('#', '-') // pls replace result.battletag with input from index.html(currently dont know how)
+var battletag = document.getElementById("searchinput").value;
+ var replace = battletag.replace('#', '-') // pls replace result.battletag with input from index.html(currently dont know how)
 request('https://owapi.net/api/v2/u/' + replace + '/stats/competitive', function(error, response, body) { // might need to change with our own server
     if (!error && response.statusCode == 200) {
 var data = JSON.parse(body);
         var levelreall = data.overall_stats.prestige * 100 + data.overall_stats.level
-        /* explosion of variable incoming, idk why but I think it will be more easy to put it in the user.html 
+        /* explosion of variable incoming, idk why but I think it will be more easy to put it in the user.html
         dont forget "stats for" which is var replace and "level" with var levelreall*/
-        
+
         var comprank = data.overall_stats.comprank + " SR"
         var total_game = "Total of game(s) played" + data.overall_stats.games + " SR"
         var games_won = "Game(s) won: " + data.overall_stats.wins
@@ -15,6 +17,7 @@ var data = JSON.parse(body);
         var winrate = "Winrate: " + data.overall_stats.win_rate
     }
     else if (!error && response.statusCode == 404) {
-        // put some redirect to an not found player page
+      window.location.assign(./notfound.html)
     }
 });
+}
