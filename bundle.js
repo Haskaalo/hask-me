cf91186eef8d8d7469f4e28272b7c1f9d3ea74fd
Haskaalo <haskaalo@outlook.com>
2017-01-07 18:18:29 -0500

    fixed some typo

--- bundle.js
+++ bundle.js
@@ -59535,7 +59535,7 @@ module.exports={
         "spec": ">=2.3.0 <2.4.0",
         "type": "range"
       },
-      "C:\\Users\\Haska\\Documents\\project-owstats\\node_modules\\request"
+      "C:\\Users\\Haska\\Documents\\hask-me\\node_modules\\request"
     ]
   ],
   "_from": "tough-cookie@>=2.3.0 <2.4.0",
@@ -59569,7 +59569,7 @@ module.exports={
   "_shasum": "f081f76e4c85720e6c37a5faced737150d84072a",
   "_shrinkwrap": null,
   "_spec": "tough-cookie@~2.3.0",
-  "_where": "C:\\Users\\Haska\\Documents\\project-owstats\\node_modules\\request",
+  "_where": "C:\\Users\\Haska\\Documents\\hask-me\\node_modules\\request",
   "author": {
     "name": "Jeremy Stashewsky",
     "email": "jstashewsky@salesforce.com"
@@ -62670,34 +62670,36 @@ arguments[4][167][0].apply(exports,arguments)
  var request = require('request');
 function search() {
 var battletag = document.getElementById("search").value;
- var replace = battletag.replace('#', '-') // pls replace result.battletag with input from index.html(currently dont know how)
+ var replace = battletag.replace('#', '-')
 request('https://owapi.net/api/v2/u/' + replace + '/stats/competitive', function(error, response, body) { // Sorry OWAPI :3, ill do my own server one day :3
     if (!error && response.statusCode == 200) {
 var data = JSON.parse(body);
-        var userlevel = "Level: " + levelreall
         var user = "Looked for: " + battletag
         var levelreall = data.overall_stats.prestige * 100 + data.overall_stats.level
+		var userlevel = "Level: " + levelreall
         var comprank = data.overall_stats.comprank + " SR"
-        var total_game = "Total of game(s) played" + data.overall_stats.games + " SR"
+        var total_game = "Total of game(s) played: " + data.overall_stats.games + " SR"
         var games_won = "Game(s) won: " + data.overall_stats.wins
-        var games_lost = "Game(s) lost" + data.overall_stats.losses
-        var winrate = "Winrate: " + data.overall_stats.win_rate
-		
+        var games_lost = "Game(s) lost: " + data.overall_stats.losses
+        var winrate = "Winrate: " + data.overall_stats.win_rate + "%"
+
 		document.getElementById("battletag").innerHTML = user;
-		document.getElementById("level").innerHTML = levelreall;
+		document.getElementById("level").innerHTML = userlevel;
 		document.getElementById("comprank").innerHTML = comprank;
 		document.getElementById("totalgame").innerHTML = total_game;
 		document.getElementById("gamewon").innerHTML = games_won;
 		document.getElementById("gamelost").innerHTML = games_lost;
 		document.getElementById("winrate").innerHTML = winrate;
-		
-		
+
+
         console.log("Looked for: " + battletag);
     }
     else if (!error && response.statusCode == 404) {
-      window.location.replace("https://hask.me/notfound.html");
+      document.getElementById("not_found").innerHTML = "Did not found the user";
+	  console.log("test")
     }
 });
 }
 window.search = search;
+
 },{"request":243}]},{},[296]);
--- stats.js
+++ stats.js
@@ -2,32 +2,33 @@
  var request = require('request');
 function search() {
 var battletag = document.getElementById("search").value;
- var replace = battletag.replace('#', '-') // pls replace result.battletag with input from index.html(currently dont know how)
+ var replace = battletag.replace('#', '-')
 request('https://owapi.net/api/v2/u/' + replace + '/stats/competitive', function(error, response, body) { // Sorry OWAPI :3, ill do my own server one day :3
     if (!error && response.statusCode == 200) {
 var data = JSON.parse(body);
-        var userlevel = "Level: " + levelreall
         var user = "Looked for: " + battletag
         var levelreall = data.overall_stats.prestige * 100 + data.overall_stats.level
+		var userlevel = "Level: " + levelreall
         var comprank = data.overall_stats.comprank + " SR"
-        var total_game = "Total of game(s) played" + data.overall_stats.games + " SR"
+        var total_game = "Total of game(s) played: " + data.overall_stats.games + " SR"
         var games_won = "Game(s) won: " + data.overall_stats.wins
-        var games_lost = "Game(s) lost" + data.overall_stats.losses
-        var winrate = "Winrate: " + data.overall_stats.win_rate
-		
+        var games_lost = "Game(s) lost: " + data.overall_stats.losses
+        var winrate = "Winrate: " + data.overall_stats.win_rate + "%"
+
 		document.getElementById("battletag").innerHTML = user;
-		document.getElementById("level").innerHTML = levelreall;
+		document.getElementById("level").innerHTML = userlevel;
 		document.getElementById("comprank").innerHTML = comprank;
 		document.getElementById("totalgame").innerHTML = total_game;
 		document.getElementById("gamewon").innerHTML = games_won;
 		document.getElementById("gamelost").innerHTML = games_lost;
 		document.getElementById("winrate").innerHTML = winrate;
-		
-		
+
+
         console.log("Looked for: " + battletag);
     }
     else if (!error && response.statusCode == 404) {
-      window.location.replace("https://hask.me/notfound.html");
+      document.getElementById("not_found").innerHTML = "Did not found the user";
+	  console.log("test")
     }
 });
 }
