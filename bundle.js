886ff6d532df405151335470040000f36992f2da
Haskaalo <haskaalo@outlook.com>
2017-01-07 15:42:56 -0500

    made the website usable

--- bundle.js
+++ bundle.js
@@ -62671,22 +62671,31 @@ arguments[4][167][0].apply(exports,arguments)
 function search() {
 var battletag = document.getElementById("search").value;
  var replace = battletag.replace('#', '-') // pls replace result.battletag with input from index.html(currently dont know how)
-request('https://owapi.net/api/v2/u/' + replace + '/stats/competitive', function(error, response, body) { // might need to change with our own server
+request('https://owapi.net/api/v2/u/' + replace + '/stats/competitive', function(error, response, body) { // Sorry OWAPI :3, ill do my own server one day :3
     if (!error && response.statusCode == 200) {
 var data = JSON.parse(body);
+        var userlevel = "Level: " + levelreall
+        var user = "Looked for: " + battletag
         var levelreall = data.overall_stats.prestige * 100 + data.overall_stats.level
-        /* explosion of variable incoming, idk why but I think it will be more easy to put it in the user.html
-        dont forget "stats for" which is var replace and "level" with var levelreall*/
-
         var comprank = data.overall_stats.comprank + " SR"
         var total_game = "Total of game(s) played" + data.overall_stats.games + " SR"
         var games_won = "Game(s) won: " + data.overall_stats.wins
         var games_lost = "Game(s) lost" + data.overall_stats.losses
         var winrate = "Winrate: " + data.overall_stats.win_rate
-        console.log("If you see this, it might have work...");
+		
+		document.getElementById("battletag").innerHTML = user;
+		document.getElementById("level").innerHTML = levelreall;
+		document.getElementById("comprank").innerHTML = comprank;
+		document.getElementById("totalgame").innerHTML = total_game;
+		document.getElementById("gamewon").innerHTML = games_won;
+		document.getElementById("gamelost").innerHTML = games_lost;
+		document.getElementById("winrate").innerHTML = winrate;
+		
+		
+        console.log("Looked for: " + battletag);
     }
     else if (!error && response.statusCode == 404) {
-      window.location.assign('./notfound.html');
+      window.location.replace("https://hask.me/notfound.html");
     }
 });
 }
