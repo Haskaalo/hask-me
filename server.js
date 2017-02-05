ece667fc6baae1179d58f13107f2f02da3944387
Trev <Haskaalo@users.noreply.github.com>
2017-02-05 15:42:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ makenew();
   });
 
 
-  server.listen(3000, 'localhost');
+  server.listen(3000);
   server.on('listening', function() {
       console.log('Express server started on port %s at %s', server.address().port, server.address().address);
   });
