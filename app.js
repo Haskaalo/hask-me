15401fe5fe30b4487302a4537493cbd1b56b772b
Haskaalo <haskaalo@outlook.com>
2017-05-22 20:21:23 -0400

    overank.me is back

--- app.js
+++ app.js
@@ -5,17 +5,6 @@ var http = require('http');
 var server = http.createServer(app);
 var compression = require('compression');
 const fs = require('fs');
-var randomstring = require('randomstring');
-var dustDB = require('dustdb');
-var randomnum = randomstring.generate({
-  length: 7,
-  charset: 'alphanumeric'
-});
-
-if (!fs.existsSync('./tmp')){
-  fs.mkdirSync('./tmp');
-  fs.mkdirSync('./tmp/linkshort');
-}
 
 app.use(compression())
 // Store all .html in views folder + static
@@ -24,8 +13,6 @@ app.use(express.static(path.join(__dirname, 'views'),{extensions:['html']}));
 app.use(express.static(__dirname + '/scripts'));
 //Store all JS and CSS in Scripts folder.
 app.use(express.static(__dirname + '/imgs'));
-// link Store
-app.use(express.static(path.join(__dirname, '/tmp/linkshort'),{extensions:['html']}));
 
 //Store all imgs and Video on imgs Folder
 app.set('view engine', 'html');
@@ -33,45 +20,6 @@ app.use(express.static(__dirname + '/intriguant'));
 
 app.use(express.static(path.join(__dirname, './intriguant/views'),{extensions:['html']}));
 
-app.get('/intriguant', function (req, res) {
-  res.render('./intriguant.html')
-})
-
-app.get('/', function (req, res) {
-  res.render('./index.html');
-})
-
-// LINK SHORT start
-function makenew() {
-  return randomnum = randomstring.generate({
-  length: 7,
-  charset: 'alphanumeric'
-});
-}
-app.get('/linkshortapi?:id', function (req, res, next) {
-var decode = decodeURI(req.query.id);
-fs.readFile('./keys/' + req.headers['x-access-key'], (err) => {
-if (err) {
-  res.status(403).send('403: Not Authorized')
-}
-else {
-dustDB.createfile('./tmp/linkshort/', randomnum, decode);
-res.send('hask.me/l/' + randomnum);
-makenew();
-}
-});
-})
-
-app.get('/l/:link', function (req, res, next) {
-  fs.readFile('./tmp/linkshort/' + req.params.link, (err, data) => {
-    if (err) {
-      res.status(404).send('404: Page Not Found')
-    }
-    else {
-      res.redirect(301, data);
-    }
-  });
-})
 // LINK SHORT END
 
   // Handle 404
